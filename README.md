f0d0e339d71fb55629de05f6d51132f7da06ec66
Bored-deve <rafayabbas811@gmail.com>
2025-02-23 22:31:49 +0500

    Project init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# GoogleDrive
+A google drive project made on nodejs express and mongodb.
+
+if you have any recommendations please tell me!!
+
+___Rafay Abbas
---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});   
+
+app.get('/about', (req, res) => {
+    res.send('About!');
+  });
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
