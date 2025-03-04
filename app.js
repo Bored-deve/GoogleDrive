00cda045209c765e0e51908ace505d7d54751fdd
Bored-deve <rafayabbas811@gmail.com>
2025-03-05 01:17:46 +0500

    made a gitignore env and connected to db using env

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan')
 const userRouter = require('./routes/user.routes');
-
+const dotenv = require('dotenv');
 app.use(morgan('dev')) //external middleware to show what's going on in the console
 app.use(express.json()) // internal middleware to make my server able to understand json data
 app.use(express.urlencoded({ extended: true })) // used form submission
 app.use(express.static("public")) // used for static files
+
+dotenv.config();
 
 app.set('view engine', 'ejs');
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.get('/register', (req, res) => {
-  res.render('register');
-});
+// user register
 
-router.post('/register-user', (req, res) => {
-  console.log(req.body);
-  res.send('User registered');
-});
+router.get('/register',
+  (req, res) => {
+    res.render('register');
+  });
+
+router.post('/register-user',
+  body('user_email').trim().isEmail().isLength({ min: 13 }),
+  body('user_password').trim().isLength({ min: 5 }),
+  body('user_name').trim().isLength({ min: 3 }),
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: 'Invalid data'
+      })
+    }
+
+    else {
+      res.send('data recieved nigga');
+    }
+
+    console.log(req.body);
+  });
 
 module.exports = router;
